b005f93c910ce49814028e36762dd5eec228bf5c
Tobias Renal <1872015@maranatha.ac.id>
2020-12-31 11:11:25 +0700

    tes

--- src/sample/Controller/Controller.java
+++ src/sample/Controller/Controller.java
@@ -1,4 +1,25 @@
 package sample.Controller;
 
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+    public TextField SearchBar;
+    public Button Search;
+    public Button Account;
+    public TableView TabelLagu;
+    public TableColumn KolomJudul;
+    public TableColumn KolomAlbum;
+    public TableColumn KolomArtist;
+    public ImageView CoverLagu;
+    public Label JudulLagu;
+    public Label ArtisLagu;
+    public Button ShuffleButton;
+    public Button PrevButton;
+    public Button PlayButton;
+    public Button NextButton;
+    public Button RepeatButton;
+    public Label ProgressLagu;
+    public Label SisaWaktu;
+    public Slider VolumeSlider;
 }
