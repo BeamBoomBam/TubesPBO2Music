9bcd19cb46a052152ce45f1815b2fbd851310807
josrel <64628213+josrel@users.noreply.github.com>
2021-01-11 03:01:28 +0700

    stage style

--- src/sample/Main/Main.java
+++ src/sample/Main/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -13,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("../View/LoginPage.fxml"));
         primaryStage.setTitle("Musique streaming");
         primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 
